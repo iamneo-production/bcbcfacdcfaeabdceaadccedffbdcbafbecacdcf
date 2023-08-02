dcd099e0277c93bb2a5739f2c08c7155f26f3df4
Iamneo <techadmin@iamneo.ai>
2023-08-02 18:35:53 +0000

    sonar please do the needful

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -4,11 +4,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <d>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
